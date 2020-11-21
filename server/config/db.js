da5d4b7a7f4b0affeb75eb1521c05822ace181d0
veyselboybay <veysel.boybay@outlook.com>
2020-11-21 18:48:22 -0500

    db source updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/book_store",
-    //"URI":"mongodb+srv://veyselboybay:m6NuKukJTapbE6FH@mongodbserver.dj034.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI":"mongodb://localhost/book_store",
+    "URI":"mongodb+srv://veyselboybay:m6NuKukJTapbE6FH@mongodbserver.dj034.mongodb.net/book_store?retryWrites=true&w=majority",
     "Secret":'SomeSecret'
 }
